ef0040c8226f70694785c1a6fa21eb8549f01509
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2024-08-06 00:11:49 +0300

    update

--- dijkstra.py
+++ dijkstra.py
@@ -13,7 +13,7 @@ class Edge:
         Args:
             source_vertex (Vertex): The source vertex of the edge.
             target_vertex (Vertex): The target vertex of the edge.
-            weight (int): The weight of the edge.
+            weight (int): The non-negative weight of the edge.
         """
         self.source_vertex = source_vertex
         self.target_vertex = target_vertex
@@ -42,16 +42,17 @@ class Vertex:
         """
         return self.min_distance < other_vertex.min_distance
 
-    def add_edge(self, weight, target_vertex):
+    def add_edge(self, target_vertex, weight):
         """
         Adds a new edge from the current vertex to the target vertex.
 
         Args:
-            weight (int): The weight of the edge.
             target_vertex (Vertex): The target vertex of the edge.
+            weight (int): The weight of the edge.
         """
         new_edge = Edge(self, target_vertex, weight)
         self.neighbours.append(new_edge)
+
 
 class DijkstraAlgorithm:
     def __init__(self):
@@ -70,27 +71,18 @@ class DijkstraAlgorithm:
         """
         end_vertex = target_vertex
     
-        print(
-              f"Calculating the shortest path from {source_vertex} to {end_vertex}"
-        )
+        print(f"Calculating the shortest path from {source_vertex} to {end_vertex}")
 
+        # Calculate the optimal tentative distances.
         self.calculate(source_vertex)
 
-        stack = []
+        # Get the shortest path backward from the destination.
+        shortest_path = []
         while end_vertex is not None:
-            stack.append(end_vertex)
+            shortest_path.append(end_vertex)
             end_vertex = end_vertex.predecessor
         
-        shortest_path = ""            
-        while stack:
-            vertex = stack.pop()
-            shortest_path += f"{vertex} -> "
-        
-        # Remove the last three elements, i.e., "-> ".
-        shortest_path = shortest_path[:-3]
-
         return shortest_path, target_vertex.min_distance
-
 
     def calculate(self, start_vertex):
         """
@@ -106,40 +98,34 @@ class DijkstraAlgorithm:
             # Pop the vertex with min distance from the heap.
             current_vertex = heapq.heappop(self.heap)
 
-            # If the current vertex has already been visited, we don't need
-            # to process it again.
+            # If the current vertex has already been visited, it is because
+            # nodes can have multiple edges. Hence, we do not need to recompute
+            # the shortest path to this already visited node.
             if current_vertex.visited:
                 continue
 
-            # Traverse the neighbors of the current vertex.
+            # Update the tentative distances for current vertex neighbours.
             for edge in current_vertex.neighbours:
-                # Get the source and target vertices for the neighbor edge.
-                source_vertex = edge.source_vertex
-                target_vertex = edge.target_vertex
+                source_vertex, target_vertex  = edge.source_vertex, edge.target_vertex
                 
-                # Calculate the new distance.
+                # Caclulate the new distance and Check if it is less than the
+                # target neigbour vertex cost.
                 new_distance = current_vertex.min_distance + edge.weight
-
-                # Check if the newly calculated distance is less than
-                # the actual min distance for the target vertex.
                 if new_distance < target_vertex.min_distance:
-                    # Update the target vertex's min_distance to be
-                    # the new distance.
                     target_vertex.min_distance = new_distance
-                    
-                    # Update the predecessor for the target vertex to be
-                    # the current vertex since we found a shorter distance
-                    # which might be the shortest path.
                     target_vertex.predecessor = source_vertex
-                    
-                    # Add the new target vertex min distance to the heap
-                    # invariant. Note we don't update the min distance value
-                    # for the target vertex in place in the heap since
-                    # the worst would be O(n + log(n)). Instead, we add the
-                    # smaller min distance value to the heap while maintaining
-                    # the heap invariant, e.g., [F-13, F-15].
                     heapq.heappush(self.heap, target_vertex)
 
-            # Mark the current vertex as visited to avoid revisiting it
-            # since we always get the updated min distance for this vertex first.
             current_vertex.visited = True
+
+    @staticmethod
+    def print_path(path):
+        shortest_path = ""
+        min_distance = path[0].min_distance
+
+        while path:
+            vertex = path.pop()
+            shortest_path += f"{vertex} -> "
+        shortest_path = shortest_path[:-3]
+
+        print(f"Shortest path: {shortest_path}" + "\n" + f"Min Distance: {min_distance}")
